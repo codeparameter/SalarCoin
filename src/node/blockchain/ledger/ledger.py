b24696966d93ac5345dfc7adb698dc66e0843c13
codeparameter <codeparameter.com@gmail.com>
2023-09-27 03:06:08 +0330

    utxo now accepting blocks

--- src/node/blockchain/ledger/ledger.py
+++ src/node/blockchain/ledger/ledger.py
@@ -6,7 +6,7 @@ class Ledger:
 
     def __init__(self, block_header: Block, sk, vk):
         self.header = block_header
-        self.utxo: {str: Tx} = {}  # also known as memory pool
+        self.utxo: {str: Tx | Block} = {}  # also known as memory pool
         # str key in utxo is the hash pointer of a transaction
         self.sk = sk
         self.vk = vk
